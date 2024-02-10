ce23e4d1f4c2eb4145251c03b70e5a552c00f7a8
bugsounet <github@bugsounet.fr>
2024-02-10 13:05:17 +0100

    version and add new minify

--- installer/minify.js
+++ installer/minify.js
@@ -10,9 +10,17 @@ var files = [
   "../node_helper.js"
 ]
 
+let project = require("../package.json").name
+let revision = require("../package.json").rev
+let version =  require("../package.json").version
+let commentIn  = "/**"
+let commentOut = "**/"
+
 function searchFiles() {
   let components = globSync('../components/*.js')
+  let AssistantSDK = globSync('../components/AssistantSDK/*.js')
   files = files.concat(components)
+  files = files.concat(AssistantSDK)
   console.log("Found: " + files.length + " files to minify\n")
 }
 
@@ -24,6 +32,7 @@ async function minifyFiles() {
 
 function minify(file) {
   let pathResolve = path.resolve(__dirname, file)
+  let FileName = path.parse(file).base
   let error = 0
   console.log("Process File:", file)
   return new Promise((resolve,reject) => {
@@ -32,7 +41,13 @@ function minify(file) {
         entryPoints: [pathResolve],
         allowOverwrite: true,
         minify: true,
-        outfile: pathResolve
+        outfile: pathResolve,
+        banner: {
+         js: `${commentIn} ${project}\n  * File: ${FileName}\n  * Version: ${version}\n  * Revision: ${revision}\n${commentOut}`
+        },
+        footer: {
+          js: `${commentIn} Coded With Heart by bugsounet ${commentOut}`
+        }
       })
       resolve(true)
     } catch (e) {
